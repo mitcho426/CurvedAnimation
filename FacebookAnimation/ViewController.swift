42010351946d69f5f42b0adaaed9b9fc2070322f
Mitchell <Mwong@Mitchells-Mac.local>
2017-03-23 22:11:59 -0700

    Curve animation complete

--- FacebookAnimation/ViewController.swift
+++ FacebookAnimation/ViewController.swift
@@ -12,14 +12,59 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func handleTap() {
+        (0...10).forEach { (_) in
+        generatedAnimatedViews()
+        }
     }
-
+    
+    fileprivate func generatedAnimatedViews() {
+        let image = drand48() > 0.5 ? #imageLiteral(resourceName: "thumbs_up") : #imageLiteral(resourceName: "heart")
+        let imageView = UIImageView(image: image)
+        let dimension  = 20 + drand48() * 10
+        imageView.frame = CGRect(x: 0, y: 0, width: dimension, height: 30)
+        
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        
+        animation.path = customPath().cgPath
+        animation.duration = 2 + drand48() * 3
+        animation.fillMode = kCAFillModeForwards
+        animation.isRemovedOnCompletion = false
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        
+        imageView.layer.add(animation, forKey: nil)
+        view.addSubview(imageView)
+    }
 
 }
 
+
+func customPath() -> UIBezierPath {
+    let path = UIBezierPath()
+    
+    path.move(to: CGPoint(x: 0, y: 200))
+    
+    let endPoint = CGPoint(x: 400, y: 200)
+    
+    //Random Y points for random distribution of animation
+    let randomYShift = 200 + drand48() * 300
+    
+    let cp1 = CGPoint(x: 100, y: 100 - randomYShift)
+    let cp2 = CGPoint(x: 200, y: 300 + randomYShift)
+    
+    path.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+    return path
+}
+
+class CurbedView: UIView {
+    override func draw(_ rect: CGRect) {
+    
+        let path = customPath()
+        path.lineWidth = 3
+        path.stroke()
+    }
+}
