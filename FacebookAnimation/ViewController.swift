903021e56626bd1b78dfb33eb21adbb5ec910b24
Mitchell <Mwong@mitchells-mac.local>
2017-03-23 20:08:01 -0700

    Initial Commit

---
+++ FacebookAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookAnimation
+//
+//  Created by Mitchell on 3/23/17.
+//  Copyright © 2017 Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
